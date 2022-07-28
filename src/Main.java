e9b0eb04ccdc020a6c305933096a17a797cceb13
Arianof <Arian.fathi03@gmai.com>
2022-07-28 14:28:35 +0430

    main class updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name;
+        String note;
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+        note = scanner.nextLine();
+        note = scanner.nextLine();
+        notePad notePad = new notePad(name , note);
+
+        try {
+            FileOutputStream fOut = new FileOutputStream(name + ".bin");
+            ObjectOutputStream out = new ObjectOutputStream(fOut);
+            out.writeObject(notePad);
+            fOut.close();
+            out.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
