dcd690c30b36d4d344b2d69679760b5b9080cc23
Arianof <Arian.fathi03@gmai.com>
2022-07-28 14:27:05 +0430

    notePad class added

--- src/notePad.java
+++ src/notePad.java
@@ -1,2 +1,37 @@
-public class notePad {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class notePad implements Serializable {
+    private String name;
+    private String note;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public notePad(String name, String note) {
+        this.name = name;
+        this.note = note;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return "notePad{" +
+                "name='" + name + '\'' +
+                ", note='" + note + '\'' +
+                '}';
+    }
 }
