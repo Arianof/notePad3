b17fa4cf535e608dd0fa5e0e67520f09dca6974e
Arianof <Arian.fathi03@gmai.com>
2022-07-29 14:00:20 +0430

    feat: delete method added

--- src/Main.java
+++ src/Main.java
@@ -89,10 +89,29 @@ public class Main {
             i = 0;
             for (notePad no: notePadArrayList){
                 if(in.equals(no.getName())){
+                    File file = new File("C:\\Users\\arian\\Documents\\uni\\Java\\notePad3\\" + no.getName() + ".bin");
+                    file.delete();
                     notePadArrayList.remove(i);
-                }
+                    stringArrayList.remove(i);
+                    break;
+                    }
                 i++;
             }
+            File file = new File("C:\\Users\\arian\\Documents\\uni\\Java\\notePad3\\names.txt");
+            file.delete();
+            try{
+                FileWriter fileWriter = new FileWriter("names.txt");
+                for (String s : stringArrayList){
+                    fileWriter.write(s);
+                    fileWriter.write("\n");
+                }
+                fileWriter.close();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+
+
 
         }
 
